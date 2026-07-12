2f7e773671c57c729a27172f4b1bfcee4f6464c8
legerise <legend9266@gmail.com>
2026-07-12 08:40:39 +0500

    domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://ez-clip.ovh',
+  site: 'https://legerise.github.io',
+  base: '/the-code-insights',
   devToolbar: { enabled: false },
   integrations: [sitemap()],
   markdown: {
